7b98125ef522b17c92d5d38eaff5a597aa9019b5
sdonkey <wangjr129@163.com>
2020-10-13 16:35:56 +0800

    init

---
+++ lethe/src/Test.php
@@ -0,0 +1,17 @@
+<?php
+namespace Lethe;
+/* **********************************************************
+ *
+ * @author: wangjianrong<wangjr129@163.com>
+ * @Date: 2020-10-10 16:39
+ * @copyright Copyright 2018-2023 © wangjr129@163.com All rights reserved.
+ * *********************************************************** */
+
+class Test
+{
+
+    public function test(){
+        echo "aaa";
+    }
+
+}
