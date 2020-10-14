1ee58c328cb6b1bc443ea3eed77a84afc2dcec45
sdonkey <wangjr129@163.com>
2020-10-14 21:11:42 +0800

    test2

--- lethe/src/Test.php
+++ lethe/src/Test.php
@@ -2,7 +2,7 @@
 namespace Lethe;
 /* **********************************************************
  *
- * @author: wangjianrong<wangjr129@163.com>
+ * @author: wangjr<wangjr129@163.com>
  * @Date: 2020-10-10 16:39
  * @copyright Copyright 2018-2023 © wangjr129@163.com All rights reserved.
  * *********************************************************** */
@@ -12,6 +12,10 @@ class Test
 
     public function test(){
         echo "aaa";
+    }
+
+    public function test2(){
+        echo "test2";
     }
 
 }
